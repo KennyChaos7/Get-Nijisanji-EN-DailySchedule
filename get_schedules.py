c27d17e690ff53f42652a3a8d5a90fbbf4001aad
HEYMEM <1791407367@qq.com>
2023-08-01 13:13:17 +0800

    基本完成，可能会后续美化

--- get_schedules.py
+++ get_schedules.py
@@ -7,6 +7,7 @@ import time
 import os
 import json
 import math
+import datetime
 # import cv2
 # import numpy as np
 from PIL import Image, ImageFont, ImageDraw
@@ -22,6 +23,7 @@ class Liver:
 
 path = os.getcwd()
 imgFileName = path + "/schedules.jpg"
+_fontType = './Silver.ttf'
 _liverList = []
 _liveListTextSize = 0
 reqUrl = 'https://dhiljqbdkw8vk.cloudfront.net/schedules'
@@ -46,30 +48,31 @@ def get():
             _scheduleThumbnails = json.loads(json.dumps(_schedule['ThumbnailDetails']))
             _headMedium = _headIconJson.get("Medium")
             _streamMediumThumbnail = json.loads(json.dumps(_scheduleThumbnails['Medium']))
-
-            _liver = Liver(
-                name = _obj['StreamerName'],
-                headIcon = _headMedium['Url'],
-                title = _schedule['Title'],
-                scheduleTime = _schedule['ScheduledStartTime'],
-                streamThumbnail = _streamMediumThumbnail['Url']
-            )
-            global _liverList
-            _liverList.clear()
-            _liverList.append(_liver)
             
-            global _liveListTextSize
-            _liveListTextSize += _liver.__dict__
-        print(_liveListTextSize)   
+            scheduleDT = datetime.datetime.strptime(str(_schedule['ScheduledStartTime']),'%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%m-%d')
+            nowDT = datetime.datetime.today().strftime('%Y-%m-%d') 
+            if(scheduleDT == nowDT):
+                _liver = Liver(
+                    name = str(_obj['StreamerName']).replace('【NIJISANJI EN】',''),
+                    headIcon = _headMedium['Url'],
+                    title = _schedule['Title'],
+                    scheduleTime = str(datetime.datetime.strptime(str(_schedule['ScheduledStartTime']),'%Y-%m-%dT%H:%M:%S.%fZ').strftime('%H:%M')),
+                    streamThumbnail = _streamMediumThumbnail['Url']
+                )
+                # print(_liver.__dict__)
+                global _liverList
+                _liverList.append(_liver)
+                
+                global _liveListTextSize
+                _liveListTextSize = _liveListTextSize + 4 + len(_liver.name) + 8 + len(_liver.heabIcon) + 5 + len(_liver.title) + 12 + len(_liver.scheduleTime) + 15 + len(_liver.streamThumbnail)
     else:
         print(response.text)        
 
+def CreatePic(margin=15,backgroundRGB=[255,255,255],fontType=_fontType,fontRGB=[0,0,0]):
 
-def CreatePic(size=[500,800],margin=50,backgroundRGB=[255,255,255],fontType=r'\\System\\Library\\Fonts\\AmericanTypewriter.ttc',fontRGB=[0,0,0]):
-
-    
-
-    size=tuple(size)
+    textList, maxSingleText, allText = decodeText()
+    print(maxSingleText)
+    size=tuple([1080, len(_liverList) * 85])
     backgroundRGB=tuple(backgroundRGB)
     fontRGB=tuple(fontRGB)
 
@@ -77,32 +80,49 @@ def CreatePic(size=[500,800],margin=50,backgroundRGB=[255,255,255],fontType=r'\\
     iwidth, iheight = image.size # 获取画布高宽
 
     # 计算字节数，GBK编码下汉字双字，英文单字。都转为双字计算
-    size=len(_liveListTextSize.encode('gbk'))/2
+    size=len(maxSingleText.encode('utf-8'))/3
     # 计算字体大小，每两个字号按字节长度翻倍。
-    fontSize=math.ceil((iwidth-(margin*2))/size)
-
+    fontSize=30#math.ceil((iwidth-(margin*2))/size)
+    print(fontSize)
+    
     font = ImageFont.truetype(fontType, fontSize) # 设置字体及字号
     draw = ImageDraw.Draw(image)
-
-    fwidth, fheight = draw.textsize(_liveListTextSize, font) # 获取文字高宽
-    owidth, oheight = font.getoffset(_liveListTextSize)
+    
+    fwidth, fheight = draw.textsize(allText, font) # 获取文字高宽
+    owidth, oheight = font.getoffset(allText)
 
     fontx = (iwidth - fwidth - owidth) / 2
-    fonty = (iheight - fheight - oheight) / 2
+    # fonty = (iheight - fheight - oheight) / 4
+    fonty = fontSize + margin * 2
 
-    # draw.text((fontx, fonty), text, fontRGB, font)
-
+    draw.text((iwidth / 3, margin), str(datetime.datetime.today().strftime('%Y - %m - %d')), fontRGB, font)
+    for tmpText in textList:
+        draw.text((fontx, fonty), tmpText, fontRGB, font)
+        fonty += fontSize * 2 + margin
+        # print(tmpText)
+        
     image.save(imgFileName) # 保存图片        
 
-def get_data_task():
-    # print("start new round ....")
-    get()
-    # CreatePic()
-    # print("finish this round ....")
 
-get_data_task()
-# schedule.every(10).seconds.do(get_data_task)
-# schedule.every(15).minutes.do(get_data_task)
+def decodeText():
+    textList = []
+    maxSingleText = ""
+    allText = ""
+    _liverList.sort(key=takeSecond)
+    for _liver in _liverList:
+        # print(_liver.__dict__)
+        tmpText = _liver.scheduleTime + " " + _liver.name + "\n      " + _liver.title + "\n"
+        textList.append(tmpText)
+        allText += tmpText
+        if (len(maxSingleText) < len(tmpText)):
+            maxSingleText = tmpText
+    return textList, maxSingleText, allText
+
+def takeSecond(elem):
+    return elem.scheduleTime
+
+get()
+CreatePic()
 # schedule.every(12).hours.do(get_data_task)
 # os.popen(openServer)
 # print("start schedule task ....")
