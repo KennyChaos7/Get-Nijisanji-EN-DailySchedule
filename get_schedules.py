a522486337a1a00793254d686fb972de2011ce7e
ka <danal25191@inkiny.com>
2023-10-07 16:07:59 +0800

    更新部分样式

--- get_schedules.py
+++ get_schedules.py
@@ -12,20 +12,22 @@ import datetime
 from PIL import Image, ImageFont, ImageDraw
 
 class Liver:
-    def __init__(self, name, headIcon, title, scheduleTime, streamThumbnail, streamUrl):
+    def __init__(self, name, head_icon, title, schedule_time, stream_thumbnail, stream_url):
         self.name = name
-        self.heabIcon = headIcon
+        self.headIcon = head_icon
         self.title = title
-        self.scheduleTime = scheduleTime
-        self.streamThumbnail = streamThumbnail
-        self.streamUrl = streamUrl
+        self.scheduleTime = schedule_time
+        self.streamThumbnail = stream_thumbnail
+        self.streamUrl = stream_url
+    def __str__(self):
+        return self.name + " - " + self.headIcon + " - " + self.title + " - " + self.scheduleTime + " - " + self.streamThumbnail + " - " + self.streamUrl
 
 
 path = os.getcwd()
 imgFileName = path + "/schedules.jpg"
 _fontType = './silver.ttf'
-_liverList = []
-_liveListTextSize = 0
+_liver_list = list()
+_live_list_text_size = 0
 reqUrl = 'https://dhiljqbdkw8vk.cloudfront.net/schedules'
 reqParams = {
     
@@ -37,9 +39,10 @@ reqHeaders = {
         'Accept-Language': 'zh-CN,zh;q=0.9'
     }
 
-def get():
+def fun_get():
     response = requests.get(url = reqUrl, params = reqParams, headers = reqHeaders)
-    if(response.status_code == 200):
+    global _liver_list, _live_list_text_size
+    if response.status_code == 200:
         _array = json.loads(response.text)
         for _obj in _array: 
             _headIconJson = json.loads(json.dumps(_obj['StreamerThumbnails']))
@@ -49,35 +52,34 @@ def get():
                 if 'ScheduledStartTime' in _schedule:
                     _videoId = json.loads(json.dumps(_schedule['VideoId']))
                     _scheduleThumbnails = json.loads(json.dumps(_schedule['ThumbnailDetails']))
-                    _schedule['ScheduledStartTime'] = datetime.datetime.strptime(str(_schedule['ScheduledStartTime']),'%Y-%m-%dT%H:%M:%S.%fZ') + datetime.timedelta(hours=8)
-                    scheduleDT = _schedule['ScheduledStartTime'].strftime('%Y-%m-%d')                
-                    nowDT = datetime.datetime.today().strftime('%Y-%m-%d') 
-                    if(scheduleDT == nowDT):
+                    _schedule['ScheduledStartTime'] = datetime.datetime.strptime(str(_schedule['ScheduledStartTime']),'%Y-%m-%dT%H:%M:%S.%fZ') + datetime.timedelta(hours = 8)
+                    schedule_datetime = _schedule['ScheduledStartTime'].strftime('%Y-%m-%d')
+                    now_datetime = datetime.datetime.today().strftime('%Y-%m-%d')
+                    if schedule_datetime == now_datetime:
                         _liver = Liver(
                             name = str(_obj['StreamerName']).replace('【NIJISANJI EN】',''),
-                            headIcon = _headIconJson['Url'],
+                            head_icon= _headIconJson['Url'],
                             title = _schedule['Title'],
-                            scheduleTime = str(_schedule['ScheduledStartTime'].strftime('%H:%M')),
-                            streamThumbnail = _scheduleThumbnails['Url'],
-                            streamUrl = "https://youtube.com/watch?v=" + _videoId #_channelUrl + _videoId
+                            schedule_time= str(_schedule['ScheduledStartTime'].strftime('%H:%M')),
+                            stream_thumbnail= _scheduleThumbnails['Url'],
+                            stream_url="https://youtube.com/watch?v=" + _videoId #_channelUrl + _videoId
                         )
                         # print(_liver.__dict__)
-                        global _liverList
-                        _liverList.append(_liver)
-                        
-                        global _liveListTextSize
-                        _liveListTextSize = _liveListTextSize + 4 + len(_liver.name) + 8 + len(_liver.heabIcon) + 5 + len(_liver.title) + 12 + len(_liver.scheduleTime) + 15 + len(_liver.streamThumbnail)
+                        _liver_list.append(_liver)
 
-                    _liverList.sort(key=takeSecond)
+                        _live_list_text_size = _live_list_text_size + 4 + len(_liver.name) + 8 + len(_liver.headIcon) + 5 + len(_liver.title) + 12 + len(_liver.scheduleTime) + 15 + len(_liver.streamThumbnail)
+
+                        _liver_list.sort(key = fun_take_second)
 
     else:
-        print(response.text)        
+        print(response.text)
+    return _liver_list
 
-def saveToJPG(margin = 15, backgroundRGB = [255,255,255], fontType = _fontType, fontRGB = [0,0,0]):
+def fun_save_to_jpg(margin = 15, backgroundRGB = [255, 255, 255], fontType = _fontType, fontRGB = [0, 0, 0]):
 
-    textList, maxSingleText, allText = decodeText()
+    textList, maxSingleText, allText = fun_decode_text()
     # print(maxSingleText)
-    size = tuple([1080, len(_liverList) * 85])
+    size = tuple([1080, len(_liver_list) * 85])
     backgroundRGB = tuple(backgroundRGB)
     fontRGB = tuple(fontRGB)
 
@@ -85,7 +87,7 @@ def saveToJPG(margin = 15, backgroundRGB = [255,255,255], fontType = _fontType, 
     iwidth, iheight = image.size # 获取画布高宽
 
     # 计算字节数，GBK编码下汉字双字，英文单字。都转为双字计算
-    size=len(maxSingleText.encode('utf-8'))/3
+    size = len(maxSingleText.encode('utf-8'))/3
     # 计算字体大小，每两个字号按字节长度翻倍。
     fontSize = 30#math.ceil((iwidth-(margin*2))/size)
     # print(fontSize)
@@ -100,7 +102,7 @@ def saveToJPG(margin = 15, backgroundRGB = [255,255,255], fontType = _fontType, 
     # fonty = (iheight - fheight - oheight) / 4
     fonty = fontSize + margin * 2
 
-    draw.text((iwidth / 3, margin), str(datetime.datetime.today().strftime('%Y - %m - %d')) + " - 今天直播人数 : " + str(len(_liverList)), fontRGB, font)
+    draw.text((iwidth / 3, margin), str(datetime.datetime.today().strftime('%Y - %m - %d')) + " - 今天直播人数 : " + str(len(_liver_list)), fontRGB, font)
     for tmpText in textList:
         draw.text((fontx, fonty), tmpText, fontRGB, font)
         fonty += fontSize * 2 + margin
@@ -108,46 +110,49 @@ def saveToJPG(margin = 15, backgroundRGB = [255,255,255], fontType = _fontType, 
         
     image.save(imgFileName) # 保存图片        
 
-def decodeText():
-    textList = []
-    maxSingleText = ""
-    allText = ""
-    _liverList.sort(key=takeSecond)
-    for _liver in _liverList:
+def fun_decode_text():
+    text_list = []
+    max_single_text = ""
+    all_text = ""
+    _liver_list.sort(key=fun_take_second)
+    for _liver in _liver_list:
         # print(_liver.__dict__)
-        tmpText = _liver.scheduleTime + " " + _liver.name + "\n      " + _liver.title + "\n"
-        textList.append(tmpText)
-        allText += tmpText
-        if (len(maxSingleText) < len(tmpText)):
-            maxSingleText = tmpText
-    return textList, maxSingleText, allText
+        tmp_text = _liver.scheduleTime + " " + _liver.name + "\n      " + _liver.title + "\n"
+        text_list.append(tmp_text)
+        all_text += tmp_text
+        if len(max_single_text) < len(tmp_text):
+            max_single_text = tmp_text
+    return text_list, max_single_text, all_text
 
-def takeSecond(elem):
+def fun_take_second(elem):
     return elem.scheduleTime
 
-def createHtml():
-    h1 = str(datetime.datetime.today().strftime('%Y - %m - %d')) + " - 今天直播人数 : " + str(len(_liverList))
+def fun_create_html():
+    h1 = str(datetime.datetime.today().strftime('%Y - %m - %d')) + " - 今天直播人数 : " + str(len(_liver_list))
     html = """
-    <h1 align="center">%s</h1>
-    """%(h1)
-    f = open('nijisan_en_daily.html','w')
-    a_tag_list = []
-    for _liver in _liverList:
+    <!DOCTYPE html>
+    <html lang="en">
+    <body align="center">
+    <h1>%s</h1>
+    """ % h1
+    for _liver in _liver_list:
         a_tag = """
-        <h3 align="center">%s</h3>
-        <h5 align="center">%s  <a href="%s">LINK</a>
+        <h2>%s</h2>
+        <h3>%s</h3>  <br/>
+        <a href="%s"><img src="%s" alt="%s"/> <br/>
+        ⬆ CLICK LINK</a>
         <br/>
-        <img src="%s"/>
-        </h5>
-        """%(_liver.scheduleTime + "   " + _liver.name, _liver.title, _liver.streamUrl, _liver.streamThumbnail)
-        html+=a_tag
+        """%(_liver.scheduleTime + "   " + _liver.name, _liver.title, _liver.streamUrl, _liver.streamThumbnail, _liver.name)
+        html += a_tag
+    html += "</body><html>"
+    f = open('nijisan_en_daily.html', 'w')
     f.write(html)
     f.close()
 
 def main():
-    get()
-    saveToJPG()
-    createHtml()
+    print(fun_get())
+    fun_save_to_jpg()
+    fun_create_html()
 
 if __name__ == '__main__':
     main()
